a9b1615101a0cb62fc4c767bf25df19352ad89c6
Avirup-Dey <deyavirup01@gmail.com>
2026-05-18 20:38:03 +0530

    Add files via upload

---
+++ Online Examination System/database/db.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+ mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+
+connection.connect((err) => {
+    if (err) {
+        console.log("Database connection failed:", err);
+    } else {
+        console.log("MySQL Connected");
+    }
+});
+
+module.exports = connection;
+
